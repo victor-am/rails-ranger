af546d76ce2d539814fa92c9789e280469100636
victor-am <victor.marques@plataformatec.com>
2017-05-20 18:32:51 -0300

    Initial commit

---
+++ rails-ranger.js
@@ -0,0 +1,26 @@
+require Axios from 'axios'
+
+class RailsRanger {
+  constructor() {
+    // Add config for hostname
+    // Add config for headers
+    // Add config for timeout
+    // Add config for namespaces
+    this.client = Axios.create({})
+  }
+
+  post(path, params) {
+    return this.client.post(path, params)
+  }
+
+  get(path, pathParams, queryParams) {
+
+    let processedPath = path
+
+    pathParams.forEachWithIndex((paramName, paramValue) => {
+      processedPath = path.gsub(':' + paramName, paramValue)
+    })
+
+    return this.client.get(processedPath, queryParams)
+  }
+}
