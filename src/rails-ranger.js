db31cff5304152d9a4751740000fb6d1b2ba9620
Victor A.M <victor@victoryam.com.br>
2017-05-23 00:33:53 -0300

    Adjust style according to new linter

--- src/rails-ranger.js
+++ src/rails-ranger.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 class RailsRanger {
-  constructor(configs = {}) {
+  constructor (configs = {}) {
     // Falls back to Axios configurations
     this.client = Axios.create(configs)
   }
@@ -9,20 +9,20 @@ class RailsRanger {
   //
   // Methods
   //
-  get(path, params) {
+  get (path, params) {
     // This will translate '/users/:id' to '/users/1'
-    let prepared  = prepareArguments(path, params)
+    let prepared = prepareArguments(path, params)
     // This will inject excess params to the path like '/users/1?flag=true'
     let querifiedPath = injectPathWithQuery(prepared.path, prepared.params)
     return this.client.get(querifiedPath)
   }
 
-  post(path, params) {
+  post (path, params) {
     let prepared = prepareArguments(path, params)
     return this.client.post(prepared.path, prepared.params)
   }
 
-  delete(path, params) {
+  delete (path, params) {
     let prepared = prepareArguments(path, params)
     return this.client.delete(prepared.path, prepared.params)
   }
@@ -30,52 +30,52 @@ class RailsRanger {
   //
   // RESTful Actions
   //
-  index(...args) {
+  index (...args) {
     this.list(args)
   }
 
-  list(resource, params) {
+  list (resource, params) {
     let path = resource
     return this.get(path, params)
   }
 
-  show(resource, params) {
+  show (resource, params) {
     let path = `${resource}/:id`
     return this.get(path, params)
   }
 
-  destroy(resource, params) {
+  destroy (resource, params) {
     let path = `${resource}/:id`
     // TODO
     // Make an option for switching to get for destroy actions
     return this.delete(path, params)
   }
 
-  create(resource, params) {
+  create (resource, params) {
     let path = resource
     return this.post(path, params)
   }
 
-  update(resource, params) {
+  update (resource, params) {
     let path = `${resource}/:id`
     return this.patch(path, params)
   }
 
-  new(resource, params) {
+  new (resource, params) {
     let path = `${resource}/:id/new`
     return this.get(path, params)
   }
 
-  edit(resource, params) {
+  edit (resource, params) {
     let path = `${resource}/:id/edit`
     return this.get(path, params)
   }
 }
 
-const injectPathWithQuery = function(path, params) {
+const injectPathWithQuery = function (path, params) {
   let keyValuePairs = Object.entries(params)
-  let stringParams  = keyValuePairs.map((pair) => `${pair[0]}=${pair[1]}`)
-  let query         = stringParams.join('&')
+  let stringParams = keyValuePairs.map((pair) => `${pair[0]}=${pair[1]}`)
+  let query = stringParams.join('&')
 
   return query ? path + '?' + query : path
 }
@@ -85,8 +85,8 @@ const injectPathWithQuery = function(path, params) {
 // and return the processed path and the params left out
 // of the path
 //
-const prepareArguments = function(path, params) {
-  let processedPath   = deepClone(path)
+const prepareArguments = function (path, params) {
+  let processedPath = deepClone(path)
   let processedParams = deepClone(params)
 
   for (let key in params) {
@@ -110,7 +110,7 @@ const prepareArguments = function(path, params) {
 //
 // Returns a deep clone from a given object
 //
-const deepClone = function(object) {
+const deepClone = function (object) {
   return JSON.parse(JSON.stringify(object))
 }
 
