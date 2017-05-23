f089203c5e49327baaa611cffeba9c55d51178d1
Victor A.M <victor@victoryam.com.br>
2017-05-23 00:41:16 -0300

    Adjust setup files to new linter style

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,54 @@
-const gulp = require('gulp');
-const loadPlugins = require('gulp-load-plugins');
-const del = require('del');
-const glob = require('glob');
-const path = require('path');
-const isparta = require('isparta');
-const webpack = require('webpack');
-const webpackStream = require('webpack-stream');
+const gulp = require('gulp')
+const loadPlugins = require('gulp-load-plugins')
+const del = require('del')
+const glob = require('glob')
+const path = require('path')
+const isparta = require('isparta')
+const webpack = require('webpack')
+const webpackStream = require('webpack-stream')
 
-const Instrumenter = isparta.Instrumenter;
-const mochaGlobals = require('./test/setup/.globals');
-const manifest = require('./package.json');
+const Instrumenter = isparta.Instrumenter
+const mochaGlobals = require('./test/setup/.globals')
+const manifest = require('./package.json')
 
 // Load all of our Gulp plugins
-const $ = loadPlugins();
+const $ = loadPlugins()
 
 // Gather the library data from `package.json`
-const config = manifest.babelBoilerplateOptions;
-const mainFile = manifest.main;
-const destinationFolder = path.dirname(mainFile);
-const exportFileName = path.basename(mainFile, path.extname(mainFile));
+const config = manifest.babelBoilerplateOptions
+const mainFile = manifest.main
+const destinationFolder = path.dirname(mainFile)
+const exportFileName = path.basename(mainFile, path.extname(mainFile))
 
-function cleanDist(done) {
-  del([destinationFolder]).then(() => done());
+function cleanDist (done) {
+  del([destinationFolder]).then(() => done())
 }
 
-function cleanTmp(done) {
-  del(['tmp']).then(() => done());
+function cleanTmp (done) {
+  del(['tmp']).then(() => done())
 }
 
 // Lint a set of files
-function lint(files) {
+function lint (files) {
   return gulp.src(files)
     .pipe($.eslint())
     .pipe($.eslint.format())
-    .pipe($.eslint.failAfterError());
+    .pipe($.eslint.failAfterError())
 }
 
-function lintSrc() {
-  return lint('src/**/*.js');
+function lintSrc () {
+  return lint('src/**/*.js')
 }
 
-function lintTest() {
-  return lint('test/**/*.js');
+function lintTest () {
+  return lint('test/**/*.js')
 }
 
-function lintGulpfile() {
-  return lint('gulpfile.js');
+function lintGulpfile () {
+  return lint('gulpfile.js')
 }
 
-function build() {
+function build () {
   return gulp.src(path.join('src', config.entryFileName))
     .pipe(webpackStream({
       output: {
@@ -75,29 +75,29 @@ function build() {
     .pipe($.sourcemaps.init({loadMaps: true}))
     .pipe($.uglify())
     .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest(destinationFolder));
+    .pipe(gulp.dest(destinationFolder))
 }
 
-function _mocha() {
+function _mocha () {
   return gulp.src(['test/setup/node.js', 'test/unit/**/*.js'], {read: false})
     .pipe($.mocha({
       reporter: 'dot',
       globals: Object.keys(mochaGlobals.globals),
       ignoreLeaks: false
-    }));
+    }))
 }
 
-function _registerBabel() {
-  require('babel-register');
+function _registerBabel () {
+  require('babel-register')
 }
 
-function test() {
-  _registerBabel();
-  return _mocha();
+function test () {
+  _registerBabel()
+  return _mocha()
 }
 
-function coverage(done) {
-  _registerBabel();
+function coverage (done) {
+  _registerBabel()
   gulp.src(['src/**/*.js'])
     .pipe($.istanbul({
       instrumenter: Instrumenter,
@@ -107,26 +107,26 @@ function coverage(done) {
     .on('finish', () => {
       return test()
         .pipe($.istanbul.writeReports())
-        .on('end', done);
-    });
+        .on('end', done)
+    })
 }
 
-const watchFiles = ['src/**/*', 'test/**/*', 'package.json', '**/.eslintrc'];
+const watchFiles = ['src/**/*', 'test/**/*', 'package.json', '**/.eslintrc']
 
 // Run the headless unit tests as you make changes.
-function watch() {
-  gulp.watch(watchFiles, ['test']);
+function watch () {
+  gulp.watch(watchFiles, ['test'])
 }
 
-function testBrowser() {
+function testBrowser () {
   // Our testing bundle is made up of our unit tests, which
   // should individually load up pieces of our application.
   // We also include the browser setup file.
-  const testFiles = glob.sync('./test/unit/**/*.js');
-  const allFiles = ['./test/setup/browser.js'].concat(testFiles);
+  const testFiles = glob.sync('./test/unit/**/*.js')
+  const allFiles = ['./test/setup/browser.js'].concat(testFiles)
 
   // Lets us differentiate between the first build and subsequent builds
-  var firstBuild = true;
+  var firstBuild = true
 
   // This empty stream might seem like a hack, but we need to specify all of our files through
   // the `entry` option of webpack. Otherwise, it ignores whatever file(s) are placed in here.
@@ -155,48 +155,48 @@ function testBrowser() {
       devtool: 'inline-source-map'
     }, null, () => {
       if (firstBuild) {
-        $.livereload.listen({port: 35729, host: 'localhost', start: true});
-        gulp.watch(watchFiles, ['lint']);
+        $.livereload.listen({port: 35729, host: 'localhost', start: true})
+        gulp.watch(watchFiles, ['lint'])
       } else {
-        $.livereload.reload('./tmp/__spec-build.js');
+        $.livereload.reload('./tmp/__spec-build.js')
       }
-      firstBuild = false;
+      firstBuild = false
     }))
-    .pipe(gulp.dest('./tmp'));
+    .pipe(gulp.dest('./tmp'))
 }
 
 // Remove the built files
-gulp.task('clean', cleanDist);
+gulp.task('clean', cleanDist)
 
 // Remove our temporary files
-gulp.task('clean-tmp', cleanTmp);
+gulp.task('clean-tmp', cleanTmp)
 
 // Lint our source code
-gulp.task('lint-src', lintSrc);
+gulp.task('lint-src', lintSrc)
 
 // Lint our test code
-gulp.task('lint-test', lintTest);
+gulp.task('lint-test', lintTest)
 
 // Lint this file
-gulp.task('lint-gulpfile', lintGulpfile);
+gulp.task('lint-gulpfile', lintGulpfile)
 
 // Lint everything
-gulp.task('lint', ['lint-src', 'lint-test', 'lint-gulpfile']);
+gulp.task('lint', ['lint-src', 'lint-test', 'lint-gulpfile'])
 
 // Build two versions of the library
-gulp.task('build', ['lint', 'clean'], build);
+gulp.task('build', ['lint', 'clean'], build)
 
 // Lint and run our tests
-gulp.task('test', ['lint'], test);
+gulp.task('test', ['lint'], test)
 
 // Set up coverage and run tests
-gulp.task('coverage', ['lint'], coverage);
+gulp.task('coverage', ['lint'], coverage)
 
 // Set up a livereload environment for our spec runner `test/runner.html`
-gulp.task('test-browser', ['lint', 'clean-tmp'], testBrowser);
+gulp.task('test-browser', ['lint', 'clean-tmp'], testBrowser)
 
 // Run the headless unit tests as you make changes.
-gulp.task('watch', watch);
+gulp.task('watch', watch)
 
 // An alias of test
-gulp.task('default', ['test']);
+gulp.task('default', ['test'])
--- test/unit/rails-ranger.js
+++ test/unit/rails-ranger.js
@@ -1,25 +1,25 @@
-import RailsRanger from '../../src/rails-ranger';
+import RailsRanger from '../../src/rails-ranger'
 
 describe('railsRanger', () => {
   describe('.get', () => {
     beforeEach(() => {
-      spy(railsRanger.client, 'get');
-    });
+      spy(RailsRanger.client, 'get')
+    })
 
     it('triggers a get request through the client', () => {
-      api = new RailsRanger
+      let api = new RailsRanger()
       api.get('/users')
-      expect(railsRanger.client.get).to.have.been.calledOnce;
-    });
+      expect(RailsRanger.client.get).to.have.been.calledOnce
+    })
 
     it('interpolates parameters into the path', () => {
-      api = new RailsRanger
+      let api = new RailsRanger()
       api.get('/users/:id', { id: 1 })
-      expect(railsRanger.client.get).to.have.been.calledOnce;
-    });
+      expect(RailsRanger.client.get).to.have.been.calledOnce
+    })
 
     it('transforms remaining parameters into query params', () => {
-      expect(railsRanger.greet).to.have.always.returned('hello');
-    });
-  });
-});
+      expect(RailsRanger.greet).to.have.always.returned('hello')
+    })
+  })
+})
