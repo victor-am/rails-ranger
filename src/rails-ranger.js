e97a508247df03a619cfd22bdcc0f6b07ce9a52a
Victor A.M <victor@victoryam.com.br>
2017-05-29 01:22:48 -0300

    Add missing documentation for RailsRanger class

--- src/rails-ranger.js
+++ src/rails-ranger.js
@@ -32,7 +32,7 @@ class RailsRanger {
   /**
   * Makes a POST request to the given path with the given parameters
   * @param {string} path - The base path of the request
-  * @param {object} params - The parameters to be injected in the path (as query or replacing path segments)
+  * @param {object} params - The parameters to be injected in the path or sent in the request payload
   * @returns {Promise}
   * @example
   * let api = new RailsRanger
@@ -47,7 +47,7 @@ class RailsRanger {
   /**
   * Makes a PATCH request to the given path with the given parameters
   * @param {string} path - The base path of the request
-  * @param {object} params - The parameters to be injected in the path (as query or replacing path segments)
+  * @param {object} params - The parameters to be injected in the path or sent in the request payload
   * @returns {Promise}
   * @example
   * let api = new RailsRanger
@@ -62,7 +62,7 @@ class RailsRanger {
   /**
   * Makes a PUT request to the given path with the given parameters
   * @param {string} path - The base path of the request
-  * @param {object} params - The parameters to be injected in the path (as query or replacing path segments)
+  * @param {object} params - The parameters to be injected in the path or sent in the request payload
   * @returns {Promise}
   * @example
   * let api = new RailsRanger
@@ -89,41 +89,118 @@ class RailsRanger {
     return this.client.delete(request.path, request.params)
   }
 
+  /**
+  * Makes a GET request to the **index path** of the given resource
+  * @alias list
+  * @param {string} resource - The base path of the request
+  * @param {object} params - The parameters to be injected in the path as query
+  * @returns {Promise}
+  * @example
+  * let api = new RailsRanger
+  * api.index('users', { flag: true })
+  * //=> GET request to '/users?flag=true' path
+  */
   index (resource, params) {
     let request = this.route.index(resource, params)
     return this.client.get(request.path)
   }
 
-  list (resource, params) {
-    let request = this.route.index(resource, params)
-    return this.client.get(request.path)
+  /**
+  * An alias for the index method
+  */
+  list (...args) {
+    return this.index(...args)
   }
 
+  /**
+  * Makes a GET request to the **show path** of the given resource
+  * @param {string} resource - The base path of the request
+  * @param {object} params - The parameters to be injected in the path as query
+  * @param {number|string} params.id - The id of the resource
+  * @returns {Promise}
+  * @example
+  * let api = new RailsRanger
+  * api.show('users', { id: 1, flag: true })
+  * //=> GET request to '/users/1?flag=true' path
+  */
   show (resource, params) {
     let request = this.route.show(resource, params)
     return this.client.get(request.path)
   }
 
+  /**
+  * Makes a DELETE request to the **destroy path** of the given resource
+  * @param {string} resource - The base path of the request
+  * @param {object} params - The parameters to be injected in the path as query
+  * @param {number|string} params.id - The id of the resource
+  * @returns {Promise}
+  * @example
+  * let api = new RailsRanger
+  * api.destroy('users', { id: 1, flag: true })
+  * //=> DELETE request to '/users/1?flag=true' path
+  */
   destroy (resource, params) {
     let request = this.route.destroy(resource, params)
     return this.client.delete(request.path)
   }
 
+  /**
+  * Makes a POST request to the **create path** of the given resource
+  * @param {string} resource - The base path of the request
+  * @param {object} params - The parameters to be injected in the path as query
+  * @returns {Promise}
+  * @example
+  * let api = new RailsRanger
+  * api.create('users', { email: 'john@doe.com', password: 123456 })
+  * //=> POST request to '/users' path with the { email: 'john@doe.com', password: 123456 } parameters
+  */
   create (resource, params) {
     let request = this.route.create(resource, params)
     return this.client.post(request.path, request.params)
   }
 
+  /**
+  * Makes a PATCH request to the update path of the given resource
+  * @param {string} resource - The base path of the request
+  * @param {object} params - The parameters to be injected in the path as query
+  * @param {number|string} params.id - The id of the resource
+  * @returns {Promise}
+  * @example
+  * let api = new RailsRanger
+  * api.update('users', { id: 1, email: 'elton@doe.com' })
+  * //=> PATCH request to '/users/1' path with the { email: 'elton@doe.com' } parameters
+  */
   update (resource, params) {
     let request = this.route.update(resource, params)
     return this.client.patch(request.path, request.params)
   }
 
+  /**
+  * Makes a GET request to the new path of the given resource
+  * @param {string} resource - The base path of the request
+  * @param {object} params - The parameters to be injected in the path as query
+  * @returns {Promise}
+  * @example
+  * let api = new RailsRanger
+  * api.new('users', { flag: true })
+  * //=> GET request to '/users/new?flag=true' path
+  */
   new (resource, params) {
     let request = this.route.new(resource, params)
     return this.client.get(request.path)
   }
 
+  /**
+  * Makes a GET request to the edit path of the given resource
+  * @param {string} resource - The base path of the request
+  * @param {object} params - The parameters to be injected in the path as query
+  * @param {number|string} params.id - The id of the resource
+  * @returns {Promise}
+  * @example
+  * let api = new RailsRanger
+  * api.edit('users', { id: 1, flag: true })
+  * //=> GET request to '/users/1/edit?flag=true' path
+  */
   edit (resource, params) {
     let request = this.route.edit(resource, params)
     return this.client.get(request.path)
