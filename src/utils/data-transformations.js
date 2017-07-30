191879ae5a17a38802d2e4d0b88a437003a14083
victor-am <victor.marques@plataformatec.com>
2017-07-29 22:02:09 -0300

    Add data-transformations documentation

--- src/utils/data-transformations.js
+++ src/utils/data-transformations.js
@@ -2,6 +2,12 @@ import { camelizeKeys, decamelizeKeys } from 'humps'
 import Axios                            from 'axios'
 
 const DataTransformations = {
+  /**
+  * Data transformation function to be used by Axios to process POST, PATCH and PUT requests data
+  * @param {object} data - The data being sent through the request
+  * @param {object} headers - The headers being sent with the request
+  * @returns {string}
+  */
   prepareRequest (data, headers) {
     const defaultTransformRequest = Axios.defaults.transformRequest[0]
     const railsData               = DataTransformations.railsFormat(data)
@@ -9,6 +15,12 @@ const DataTransformations = {
     return defaultTransformRequest(railsData, headers)
   },
 
+  /**
+  * Data transformation function to be used by Axios to process response data
+  * @param {object} data - The data received through the request response in raw format
+  * @param {object} headers - The headers received with the response
+  * @returns {object}
+  */
   prepareResponse (data, headers) {
     const defaultTransformResponse = Axios.defaults.transformResponse[0]
     const jsonData                 = defaultTransformResponse(data, headers)
